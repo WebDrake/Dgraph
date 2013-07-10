30b04837f1f07cf1cf85c83832b974b2f588c2b0
Joseph Rushton Wakeling <joe@webdrake.net>
2013-07-10 23:42:05 +0200

    Sums of head/tail nodes allow easy calculation of degree.

--- dgraph/graph.d
+++ dgraph/graph.d
@@ -2,14 +2,15 @@ module dgraph.graph;
 
 import std.algorithm, std.exception, std.range;
 
-final class Graph(bool directed)
+final class Graph(bool dir)
 {
   private:
-    size_t v; // number of vertices;
     size_t[] _head;
     size_t[] _tail;
     size_t[] _indexHead;
     size_t[] _indexTail;
+    size_t[] _sumHead = [0];
+    size_t[] _sumTail = [0];
 
     void indexEdges()
     {
@@ -21,10 +22,61 @@ final class Graph(bool directed)
     }
 
   public:
+    enum bool directed = dir;
+
+    size_t vertexCount() @property const pure nothrow
+    {
+        assert(_sumHead.length == _sumTail.length);
+        return _sumHead.length - 1;
+    }
+
+    static if (!directed)
+    {
+        size_t degree(immutable size_t v)
+        {
+            return (_sumHead[v + 1] - _sumHead[v])
+                 + (_sumTail[v + 1] - _sumTail[v]);
+        }
+    }
+
+    size_t degreeIn(immutable size_t v)
+    {
+        static if (directed)
+        {
+            return _sumTail[v + 1] - _sumTail[v];
+        }
+        else
+        {
+            return degree(v);
+        }
+    }
+
+    size_t degreeOut(immutable size_t v)
+    {
+        static if (directed)
+        {
+            return _sumHead[v + 1] - _sumHead[v];
+        }
+        else
+        {
+            return degree(v);
+        }
+    }
+
+    void addVertices(immutable size_t n)
+    {
+        size_t l = _sumHead.length;
+        _sumHead.length += n;
+        _sumTail.length += n;
+        assert(_sumHead.length == _sumTail.length);
+        _sumHead[l .. $] = _sumHead[l - 1];
+        _sumTail[l .. $] = _sumTail[l - 1];
+    }
+
     void addEdge(size_t head, size_t tail)
     {
-        enforce(head < v);
-        enforce(tail < v);
+        enforce(head < this.vertexCount);
+        enforce(tail < this.vertexCount);
         static if (!directed)
         {
             if (tail < head)
@@ -36,6 +88,8 @@ final class Graph(bool directed)
         }
         _head ~= head;
         _tail ~= tail;
+        ++_sumHead[head + 1 .. $];
+        ++_sumTail[tail + 1 .. $];
         indexEdges();
     }
 
@@ -49,7 +103,8 @@ unittest
 {
     import std.stdio;
     auto g1 = new Graph!false;
-    g1.v = 10;
+    g1.addVertices(10);
+    assert(g1.vertexCount == 10);
     g1.addEdge(5, 8);
     g1.addEdge(7, 4);
     g1.addEdge(6, 9);
@@ -58,11 +113,18 @@ unittest
         writeln("\t", head, "\t", tail);
     writeln(g1._indexHead);
     writeln(g1._indexTail);
+    writeln(g1._sumHead);
+    writeln(g1._sumTail);
+    foreach(v; iota(g1.vertexCount))
+    {
+        writeln("\td(", v, ") =\t", g1.degree(v));
+    }
     writeln;
     assert(isSorted(map!(a => g1._head[g1._indexHead[a]])(iota(g1._head.length))));
 
     auto g2 = new Graph!true;
-    g2.v = 10;
+    g2.addVertices(10);
+    assert(g2.vertexCount == 10);
     g2.addEdge(5, 8);
     g2.addEdge(7, 4);
     g2.addEdge(6, 9);
@@ -71,5 +133,11 @@ unittest
         writeln("\t", head, "\t", tail);
     writeln(g2._indexHead);
     writeln(g2._indexTail);
+    writeln(g2._sumHead);
+    writeln(g2._sumTail);
+    foreach(v; iota(g2.vertexCount))
+    {
+        writeln("\td_out(", v, ") =\t", g2.degreeOut(v), "\td_in(", v, ") =\t", g2.degreeIn(v));
+    }
     assert(isSorted(map!(a => g2._head[g2._indexHead[a]])(iota(g2._head.length))));
 }
