db41b39255a7a28168d9f8cfe78f65738f3c8e33
Joseph Rushton Wakeling <joe@webdrake.net>
2013-09-12 19:20:55 +0200

    graphtest benchmarks all available graph types.

--- graphtest.d
+++ graphtest.d
@@ -20,41 +20,51 @@
              along with this program.  If not, see $(LINK http://www.gnu.org/licenses/).
 */
 
-import std.datetime, std.stdio;
+import std.datetime, std.stdio, std.typetuple;
 import dgraph.graph, dgraph.test.tests, dgraph.test.samplegraph50,
        dgraph.test.samplegraph10k;
 
 void main()
 {
-    alias Graph = CachedEdgeList!false;
-    StopWatch watch;
-
     writeln("Let's benchmark some simple graph creation scenarios.");
-    writeln;
 
-    writeln("First, a graph of 50 vertices, with edges added one at a time.");
-    writeln("This is quite quick, so we'll do it 100_001 times with the last");
-    writeln("time being verbose.");
-    watch.start;
-	foreach (immutable _; 0 .. 100_000)
+    foreach (G; TypeTuple!(IndexedEdgeList, CachedEdgeList))
     {
-		testAddEdge!(Graph, true, 0)(50, sampleGraph50);
-    }
-    testAddEdge!(Graph, true, 1)(50, sampleGraph50);
-    watch.stop;
-    writeln("Done in ", watch.peek.msecs, " ms.");
-    writeln;
+        foreach (directed; TypeTuple!(false, true))
+        {
+            alias Graph = G!directed;
+            StopWatch watch;
 
-    writeln("Now let's try a much bigger graph of 10,000 vertices, again");
-    writeln("with each edge being added one at a time.  Because it's bigger");
-    writeln("we'll only do it 1001 times, the last verbosely.");
-    watch.reset;
-    watch.start;
-    foreach (immutable _; 0 .. 1_000)
-    {
-        testAddEdge!(Graph, true, 0)(10_000, sampleGraph10k);
+            writeln;
+            writeln("Graph type: ", (Graph.directed) ? "directed " : "undirected ", Graph.stringof);
+            writeln;
+
+            writeln("First, a graph of 50 vertices, with edges added one at a time.");
+            writeln("This is quite quick, so we'll do it 100_001 times with the last");
+            writeln("time being verbose.");
+            watch.start;
+            foreach (immutable _; 0 .. 100_000)
+            {
+                testAddEdge!(Graph, true, 0)(50, sampleGraph50);
+            }
+            testAddEdge!(Graph, true, 1)(50, sampleGraph50);
+            watch.stop;
+            writeln("Done in ", watch.peek.msecs, " ms.");
+            writeln;
+
+            writeln("Now let's try a much bigger graph of 10,000 vertices, again");
+            writeln("with each edge being added one at a time.  Because it's bigger");
+            writeln("we'll only do it 1001 times, the last verbosely.");
+            watch.reset;
+            watch.start;
+            foreach (immutable _; 0 .. 1_000)
+            {
+                testAddEdge!(Graph, true, 0)(10_000, sampleGraph10k);
+            }
+            testAddEdge!(Graph, true, 1)(10_000, sampleGraph10k);
+            watch.stop;
+            writeln("Done in ", watch.peek.msecs, " ms.");
+            writeln;
+        }
     }
-    testAddEdge!(Graph, true, 1)(10_000, sampleGraph10k);
-    watch.stop;
-    writeln("Done in ", watch.peek.msecs, " ms.");
 }
